2999231f0e9f63a0c3e515f1a9e375ed0a702f2a
Kiernan Sharief <medaturd76@gmail.com>
2025-12-06 00:59:28 -0600

    Fixed output issues

--- TUI/TUIData.ts
+++ TUI/TUIData.ts
@@ -5,16 +5,16 @@ type data = {
 };
 type color = "Black" | "Red" | "Green" | "Gold" | "Blue" | "Purple" | "Cyan" | "White";
 const colorMap = {
-    "Black": "30m",
-    "Red": "31m",
-    "Green": "32m",
-    "Gold": "33m",
-    "Blue": "34m",
-    "Purple": "35m",
-    "Cyan": "36m",
-    "White": "37m"
+    "Black": { normal: "30m", bold: "40m" },
+    "Red": { normal: "31m", bold: "41m" },
+    "Green": { normal: "32m", bold: "42m" },
+    "Gold": { normal: "33m", bold: "43m" },
+    "Blue": { normal: "34m", bold: "44m" },
+    "Purple": { normal: "35m", bold: "45m" },
+    "Cyan": { normal: "36m", bold: "46m" },
+    "White": { normal: "37m", bold: "47m" }
 }
-type style = "Bold" | "Underline" | "Reset";
+type style = "Bold" | "Underline" | "Reset" | { type: "Background", color: color };
 type textOptions = {
     color?: color;
     styles?: style[];
@@ -23,26 +23,26 @@ type styledString = {
     text: string;
     styleCode?: string;
 }
-class ConsoleString {
+export class ConsoleString {
     private text: string = "";
     public length: number = 0;
 
     constructor (string: styledString[]) {
         for (const stringI of string) {
-            this.text += stringI.styleCode + stringI.text;
+            this.text += (stringI.styleCode ?? "") + stringI.text;
             this.length += stringI.text.length;
         }
     }
 
     public append(string: styledString): ConsoleString {
-        this.text += string.styleCode + string.text;
+        this.text += (string.styleCode ?? "") + string.text;
         this.length += string.text.length;
         return this;
     }
 
     public appends(string: styledString[]): ConsoleString {
         for (const stringI of string) {
-            this.text += stringI.styleCode + stringI.text;
+            this.text += (stringI.styleCode ?? "") + stringI.text;
             this.length += stringI.text.length;
         }
         return this;
@@ -66,19 +66,19 @@ class ConsoleString {
         return this.text;
     }
 }
-function textOptionsMap(txtOpt?: textOptions): string {
+export function textOptionsMap(txtOpt?: textOptions): string {
     if (!txtOpt || (!txtOpt.color && !txtOpt.styles)) return "";
-    if (!txtOpt.styles) return "\x1b[0;" + colorMap[txtOpt.color ?? "White"];
+    if (!txtOpt.styles) return "\x1b[0;" + colorMap[txtOpt.color ?? "White"].normal;
     if (txtOpt.styles?.includes("Reset")) return "\x1b[0m";
     let output = "";
     for (const style of txtOpt.styles) {
-        output += "\x1b[" + (style == "Bold" ? "1" : "4") + ";37m";
+        output += "\x1b[" + (style == "Bold" ? "1;37m" : style == "Underline" ? "4;37m" : colorMap[(style as { type: "Background", color: color }).color].bold);
     }
-    return output.substring(0, output.length-3) + colorMap[txtOpt.color ?? "White"];
+    return output + "\x1b[" + colorMap[txtOpt.color ?? "White"].normal;
 }
 
-type renderFunc = (self: TUIElementI, width: number, height: number, parent: TUI) => ConsoleString[];
-type updateFunc = (self: TUIElementI, value: string, parent?: TUI) => void;
+type renderFunc = (self: TUIElementI, width: number, height: number) => ConsoleString[];
+type updateFunc = (self: TUIElementI, value: string) => void;
 
 type funcOverride<K> = {
     func: K;
@@ -98,11 +98,11 @@ export interface TUIRowI extends TUIElementI {
 }
 
 export function genTUIRow(objects?: {element: TUIElementI, margin_left: number}[]): TUIRowI {
-    return { type: "Row", objects: objects ?? [], data: { width: objects ? objects.reduce((output, current) => output + current.margin_left + current.element.data.width, 0) : 0, height: objects ? objects.reduce((max, current) => Math.max(max, current.element.data.height), 0) : 0}, render: (self, width, height, parent) => {
+    return { type: "Row", objects: objects ?? [], data: { width: objects ? objects.reduce((output, current) => output + current.margin_left + current.element.data.width, 0) : 0, height: objects ? objects.reduce((max, current) => Math.max(max, current.element.data.height), 0) : 0}, render: (self, width, height) => {
         const output: ConsoleString[] = [];
         let targetpos = 0;
         for (const object of (self as TUIRowI).objects) {
-            const lines = object.element.render(object.element, width, height, parent);
+            const lines = object.element.render(object.element, width, height);
             targetpos += object.margin_left;
             for (let i = 0; i < lines.length; i++) {
                 if (!output[i]) output[i] = new ConsoleString([ { text: "" } ]);
@@ -112,7 +112,7 @@ export function genTUIRow(objects?: {element: TUIElementI, margin_left: number}[
             if (object.element.type == "Box") targetpos += 2;
         }
         return output;
-    }, update: (_self, _value, _parent) => null };
+    }, update: (self, value) => (self as TUIRowI).objects.forEach(object => object.element.update(object.element, value)) };
 }
 
 export interface TUIBoxI extends TUIElementI {
@@ -121,16 +121,18 @@ export interface TUIBoxI extends TUIElementI {
 }
 
 export function genTUIBox(objects?: TUIElementI[], title?: string, opt?: textOptions, updateOverride?: funcOverride<updateFunc>): TUIBoxI {
-    return { type: "Box", objects: objects ? objects : [], data: { width: objects ? objects.reduce((max, current) => Math.max(max, current.data.width) + 2, title ? title.length : 0) : title ? title.length : 0, height: (objects ? objects.length : 0) + 2 }, render: (self, width, height, parent) => {
+    return { type: "Box", objects: objects ? objects : [], data: { width: objects ? objects.reduce((max, current) => Math.max(max, current.data.width) + 2, title ? title.length : 0) : title ? title.length : 0, height: (objects ? objects.length : 0) + 2 }, render: (self, width, height) => {
         const style = textOptionsMap(opt), output = [new ConsoleString([{ text: "╭", styleCode: style }])];
         if (title) output[0].append({ text: "─" + title + "─".repeat(self.data.width - title.length - 1)});
         else output[0].append({ text: "─".repeat(self.data.width)});
         output[0].append({ text: "╮" });
-        for (const element of (self as TUIBoxI).objects) output.push(new ConsoleString([{ text: "│ " }, { text: "", styleCode: "\x1b[0m" }]).appendcs(element.render(element, width, height, parent)).append({ text: " ".repeat(self.data.width - element.data.width) + "│", styleCode: style }));
+        for (const element of (self as TUIBoxI).objects)
+            for (const line of element.render(element, width, height)) 
+                output.push(new ConsoleString([{ text: "│ " }, { text: "", styleCode: "\x1b[0m" }]).appendc(line).append({ text: " ".repeat(self.data.width - line.length - 1) + "│", styleCode: style }));
         return [...output, new ConsoleString([{ text: "╰" + "─".repeat(self.data.width) + "╯" }, { text: "", styleCode: "\x1b[0m" }])];
-    }, update: updateOverride && updateOverride.override ? updateOverride.func : (self, value, parent?) => {
-        updateOverride ? updateOverride.func(self, value, parent) : null;
-        (self as TUIBoxI).objects.forEach(object => object.update(object, value, parent));
+    }, update: updateOverride && updateOverride.override ? updateOverride.func : (self, value) => {
+        updateOverride ? updateOverride.func(self, value) : null;
+        (self as TUIBoxI).objects.forEach(object => object.update(object, value));
         self.data = {
             width: objects ? objects.reduce((max, current) => Math.max(max, current.data.width) + 2, title ? title.length : 0) : title ? title.length : 0,
             height: (objects ? objects.length : 0) + 2
@@ -144,7 +146,7 @@ export interface TUITextI extends TUIElementI {
 }
 
 export function genTUIText(value: string, opt?: textOptions, updateOverride?: funcOverride<updateFunc>): TUITextI {
-    return { type: "Text", data: { width: value.length, height: 1 }, value: value, render: (self, _width, _hight, _parent) => (self as TUITextI).value.split("\n").map((value) => new ConsoleString([{ text: value, styleCode: textOptionsMap(opt) }])).map((value, index, array) => array.length - index == 1 ? value.append({ text: "", styleCode: "\x1b[0m" }) : value), update: updateOverride ? updateOverride.func : (_self, _value, _parent) => null };
+    return { type: "Text", data: { width: value.split("\n").reduce((max, value) => Math.max(max, value.length), 0), height: value.split("\n").length }, value: value, render: (self, _width, _height) => (self as TUITextI).value.split("\n").map((value) => new ConsoleString([{ text: value, styleCode: textOptionsMap(opt) }]).append({ text: "", styleCode: "\x1b[0m" })), update: updateOverride ? updateOverride.func : (_self, _value) => null };
 }
 
 export class TUI {
@@ -156,29 +158,30 @@ export class TUI {
     }
 
     public write(message: string) {
-        Deno.stdout.write(this.encoder.encode(message));
+        Deno.stdout.writeSync(this.encoder.encode(message));
     }
 
     public start(hideCursor?: boolean): void {
         // Set terminal to raw mode
-        //Deno.stdin.setRaw(true);
+        Deno.stdin.setRaw(true);
         if (hideCursor) this.write('\x1b[?25l');
     }
 
     public render(): void {
-        console.clear();
-        this.write('\x1b[0;0H'); // Move cursor to top-left
         const {columns, rows} = Deno.consoleSize();
         const output: string[] = [];
         for (const element of this.elements)
-            element.render(element, columns, rows, this).forEach(value => output.push(value.print()));
+            element.render(element, columns, rows).forEach(value => output.push(value.print()));
+        let out = '';
         for (let i = 0; i < rows && i < output.length; i++) {
-            this.write(output[i] + "\n");
+            out += output[i] + "\n";
         }
+        console.clear();
+        this.write(out);
     }
 
     public update(value: string): void {
         for (const element of this.elements)
-            element.update(element, value, this);
+            element.update(element, value);
     }
 }
--- TUI/main.ts
+++ TUI/main.ts
@@ -1,33 +1,67 @@
-import { TUI, genTUIBox, genTUIText, TUITextI, genTUIRow } from "./TUIData.ts";
+import { TUI, genTUIBox, genTUIText, TUITextI, genTUIRow, ConsoleString, textOptionsMap } from "./TUIData.ts";
 
-const decoder = new TextDecoder(), buffer = new Uint8Array(1);
+const decoder = new TextDecoder(), buffer = new Uint8Array(4);
 let number = 0, key = '';
+let selection = 0;
 const tui = new TUI([
         genTUIBox([
                 genTUIText("Hello World!"),
                 genTUIText("You have entered 0 characters", {}, 
                     {
                         override: true, 
-                        func: (self, _value, _parent) => 
+                        func: (self, _value) => 
                             self.data.width = ((self as TUITextI).value = `You have entered ${number} characters`).length 
                     })
             ], "Test"),
         genTUIBox([
             genTUIText("Hello", { color: "Gold" })
         ], "Testing Colors", { color: "Green" }),
-        genTUIText("Hello, World!", { color: "Cyan", styles: [ "Bold", "Underline" ] }),
+        genTUIText("Hello, World!\n", { color: "Cyan", styles: [ "Bold", "Underline" ] }),
         genTUIRow([
             {
                 element: genTUIBox([
-                    genTUIText("Hellow from Row")
+                    genTUIText("Please press a key", {},
+                        {
+                            override: true,
+                            func: (self, key) => {
+                                self.data.width = ((self as TUITextI).value = "You have pressed " + key).length;
+                            }
+                        })
                 ], "Testing Rows 1"),
                 margin_left: 2
             },
             {
                 element: genTUIBox([
-                    genTUIText("Hello from Row")
+                    genTUIText("Please press enter on\noptions A, B, or C", {}, { override: true, func: (self, value) => {
+                        const s = ["A", "B", "C"][selection];
+                        if (value == "enter") self.render = (_self,_width,_height) => [new ConsoleString([{text:"You have selected:"}]), new ConsoleString([{ text: "Option " }, { text: s, styleCode: textOptionsMap({ color: "Green", styles: ["Underline"] }) }]).append({ text: "", styleCode: "\x1b[0m" })];
+                        else if (value == "delete") self.render = (self, _width, _height) => (self as TUITextI).value.split("\n").map((value) => new ConsoleString([{ text: value }]).append({ text: "", styleCode: "\x1b[0m" }));
+                    } })
                 ], "Testing Rows 2"),
                 margin_left: 5
+            },
+            {
+                element: genTUIText("\nOption C", { color: "Red" }, { override: true, func: (self, _value) => {
+                    if (selection == 2) self.render = (_self,_width,_height) => [new ConsoleString([{text:""}]), new ConsoleString([{ text: "Option C", styleCode: textOptionsMap({ color: "Green", styles: [{ type: "Background", color: "Black" }] }) }]).append({ text: "", styleCode: "\x1b[0m" })];
+                    else self.render = (_self,_width,_height) => [new ConsoleString([{text:""}]), new ConsoleString([{ text: "Option C", styleCode: textOptionsMap({ color: "Red" })}]).append({ text: "", styleCode: "\x1b[0m" })];
+                } }),
+                margin_left: 2
+            }
+        ]),
+        genTUIRow([
+            {
+                element: genTUIText("Option A", { color: "Green", styles: [{ type: "Background", color: "Black" }] }, { override: true, func: (self, _value) => {
+                    if (selection == 0) self.render = (_self,_width,_height) => [new ConsoleString([{ text: "Option A", styleCode: textOptionsMap({ color: "Green", styles: [{ type: "Background", color: "Black" }] }) }]).append({ text: "", styleCode: "\x1b[0m" })];
+                    else self.render = (_self,_width,_height) => [new ConsoleString([{ text: "Option A", styleCode: textOptionsMap({ color: "Red" })}]).append({ text: "", styleCode: "\x1b[0m" })];
+                } }),
+                margin_left: 0
+            },
+            {
+                element: genTUIText("Option B", { color: "Red" }, { override: true, func: (self, _value) => {
+                    if (selection == 1) self.render = (_self,_width,_height) => [new ConsoleString([{ text: "Option B", styleCode: textOptionsMap({ color: "Green", styles: [{ type: "Background", color: "Black" }] }) }]).append({ text: "", styleCode: "\x1b[0m" })];
+                    else self.render = (_self,_width,_height) => [new ConsoleString([{ text: "Option B", styleCode: textOptionsMap({ color: "Red" })}]).append({ text: "", styleCode: "\x1b[0m" })];
+                } }),
+                margin_left: 3
             }
         ])
     ]);
@@ -36,13 +70,41 @@ tui.start(true);
 
 while (true) {
     tui.render();
-    await Deno.stdin.read(buffer);
-    if ((key = decoder.decode(buffer)) == '\x03') { // Ctrl+C to exit
+    const read = (await Deno.stdin.read(buffer)) || 0;
+    if ((key = decoder.decode(buffer)).at(0) == '\x03') { // Ctrl+C to exit
         console.clear();
         tui.write('\x1b[?25h'); // Show cursor
         Deno.exit();
+    } else if (key.at(0) == '\x0D') {
+        number = 0;
+        key = "enter";
+    } else if (key.at(0) == '\x7F') {
+        key = "delete";
+    } else if (key.at(0) == '\x1B' && key.at(1) == '\x5B') {
+        switch (key.at(2)) {
+            case '\x41': // A
+                selection = 2;
+                key = "up arrow";
+                break;
+            case '\x42': // B
+                selection = selection != 2 ? selection : 1;
+                key = "down arrow";
+                break;
+            case '\x43': // C
+                selection = Math.min(selection + 1, 2);
+                key = "right arrow";
+                break;
+            case '\x44': // D
+                selection = Math.max(selection - 1, 0);
+                key = "left arrow";
+                break;
+            default:
+                number--;
+                key = "special";
+        }
     } else {
         number++;
-        tui.update(key);
+        key = key.slice(0, read);
     }
-};
+    tui.update(key);
+}
